e039c9a2f370a03146422c9253e480b0b66a8504
Guillaume Bouvier <bougui@mantrisse.bis.pasteur.fr>
2020-06-26 11:08:27 +0200

    new file:   generate_README.sh

---
+++ generate_README.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env zsh
+# -*- coding: UTF8 -*-
+
+# Author: Guillaume Bouvier -- guillaume.bouvier@pasteur.fr
+# https://research.pasteur.fr/en/member/guillaume-bouvier/
+# 2020-05-29 23:23:44 (UTC+0200)
+
+func runcmd () {
+    OUTPUT=$(eval $1)
+    echo "\`\`\`"
+    echo "$ $1\n"
+    echo "$OUTPUT"
+    echo "\`\`\`"
+}
+
+cat << EOF
+# Binary tree class in python
+EOF
+
+runcmd "./bintree.py"
